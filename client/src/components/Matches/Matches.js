40e30e9c9c0786f514551b17e9b601bc310de374
himanshup <himanshup@csu.fullerton.edu>
2018-09-09 14:33:42 -0700

    fix memory leak error

--- client/src/components/Matches/Matches.js
+++ client/src/components/Matches/Matches.js
@@ -3,59 +3,13 @@ import Match from "../Match/Match";
 import axios from "axios";
 
 class Matches extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = { matches: [], isMounted: false };
-  //   this.cancelTokenSource = axios.CancelToken.source();
-  // }
-
-  // componentDidMount() {
-  //   this.getMatches(this.props.date);
-  // }
-
-  // componentDidUpdate(prevProps) {
-  //   if (this.props.date !== prevProps.date) {
-  //     this.getMatches(this.props.date);
-  //   }
-  // }
-
-  // getMatches = async date => {
-  //   // use tournament id and date passed in props to find all matches on this date
-  //   // store matches and then pass to Match component to generate cards
-  //   try {
-  //     const matches = await axios.get(`/api/matches/${this.props.id}/${date}`, {
-  //       cancelToken: this.cancelTokenSource.token
-  //     });
-  //     console.log(matches)
-  //     // for (const match of matches.data) {
-  //     //   this.setState({
-  //     //     matches: this.state.matches.concat([match])
-  //     //   });
-  //     // }
-  //   } catch (err) {
-  //     if (axios.isCancel(err)) {
-  //       // ignore
-  //     } else {
-  //       throw err;
-  //     }
-  //   } finally {
-  //     this.cancelTokenSource = null;
-  //   }
-  // };
-
-  // componentWillUnmount() {
-  //   this.cancelTokenSource && this.cancelTokenSource.cancel();
-  // }
-  
   constructor(props) {
     super(props);
     this.state = { matches: [], isMounted: false };
+    this.cancelTokenSource = axios.CancelToken.source();
   }
 
   componentDidMount() {
-    this.setState({
-      isMounted: true
-    });
     this.getMatches(this.props.date);
   }
 
@@ -65,29 +19,31 @@ class Matches extends Component {
     }
   }
 
-  getMatches = date => {
+  getMatches = async date => {
     // use tournament id and date passed in props to find all matches on this date
     // store matches and then pass to Match component to generate cards
-    axios
-      .get(`/api/matches/${this.props.id}/${date}`)
-      .then(matches => {
-        for (const match of matches.data) {
-          if (this.state.isMounted) {
-            this.setState({
-              matches: this.state.matches.concat([match])
-            });
-          }
-        }
-      })
-      .catch(error => {
-        console.log(error);
+    try {
+      const matches = await axios.get(`/api/matches/${this.props.id}/${date}`, {
+        cancelToken: this.cancelTokenSource.token
       });
+      for (const match of matches.data) {
+        this.setState({
+          matches: this.state.matches.concat([match])
+        });
+      }
+    } catch (err) {
+      if (axios.isCancel(err)) {
+        // ignore
+      } else {
+        throw err;
+      }
+    } finally {
+      this.cancelTokenSource = null;
+    }
   };
 
   componentWillUnmount() {
-    this.setState({
-      isMounted: false
-    });
+    this.cancelTokenSource && this.cancelTokenSource.cancel();
   }
 
   render() {
--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "rc-collapse/assets/index.css";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import App from "./components/App/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
