2e8c709c3b73222c14dd5cee2cec98faf0747acf
himanshup <himanshup@csu.fullerton.edu>
2018-09-11 05:50:15 -0700

    change bg color

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Footer from "../Footer/Footer.js";
+// import Footer from "../Footer/Footer.js";
 
 const Home = () => {
   return (
